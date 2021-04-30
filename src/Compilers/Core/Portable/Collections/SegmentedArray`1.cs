a35c024aedebbb2d084b962e9dc4037a6d579ed8
GGG KILLER <gggkiller2@gmail.com>
2021-04-29 22:05:01 -0300

    Optimize storage for segment information
    Original commit: https://github.com/dotnet/roslyn/commit/da878489202f645d6998f9769833ffe6ca2d2459

--- src/Compilers/Core/Portable/Collections/SegmentedArray`1.cs
+++ src/Compilers/Core/Portable/Collections/SegmentedArray`1.cs
@@ -29,17 +29,29 @@ namespace Loretta.CodeAnalysis.Collections
         /// including any padding the implementation chooses to add. Specifically, array elements lie <c>sizeof</c>
         /// bytes apart.</para>
         /// </remarks>
-        private static readonly int s_segmentSize = SegmentedArrayHelper.CalculateSegmentSize(Unsafe.SizeOf<T>());
+        private static int SegmentSize
+        {
+            [MethodImpl(SegmentedArrayHelper.FastPathMethodImplOptions)]
+            get => SegmentedArrayHelper.GetSegmentSize<T>();
+        }
 
         /// <summary>
         /// The bit shift to apply to an array index to get the page index within <see cref="_items"/>.
         /// </summary>
-        private static readonly int s_segmentShift = SegmentedArrayHelper.CalculateSegmentShift(s_segmentSize);
+        private static int SegmentShift
+        {
+            [MethodImpl(SegmentedArrayHelper.FastPathMethodImplOptions)]
+            get => SegmentedArrayHelper.GetSegmentShift<T>();
+        }
 
         /// <summary>
         /// The bit mask to apply to an array index to get the index within a page of <see cref="_items"/>.
         /// </summary>
-        private static readonly int s_offsetMask = SegmentedArrayHelper.CalculateOffsetMask(s_segmentSize);
+        private static int OffsetMask
+        {
+            [MethodImpl(SegmentedArrayHelper.FastPathMethodImplOptions)]
+            get => SegmentedArrayHelper.GetOffsetMask<T>();
+        }
 
         private readonly int _length;
         private readonly T[][] _items;
@@ -58,17 +70,17 @@ namespace Loretta.CodeAnalysis.Collections
             }
             else
             {
-                _items = new T[(length + s_segmentSize - 1) >> s_segmentShift][];
+                _items = new T[(length + SegmentSize - 1) >> SegmentShift][];
                 for (var i = 0; i < _items.Length - 1; i++)
                 {
-                    _items[i] = new T[s_segmentSize];
+                    _items[i] = new T[SegmentSize];
                 }
 
                 // Make sure the last page only contains the number of elements required for the desired length. This
                 // collection is not resizeable so any additional padding would be a waste of space.
                 //
-                // Avoid using (length & s_offsetMask) because it doesn't handle a last page size of s_segmentSize.
-                var lastPageSize = length - ((_items.Length - 1) << s_segmentShift);
+                // Avoid using (length & OffsetMask) because it doesn't handle a last page size of SegmentSize.
+                var lastPageSize = length - ((_items.Length - 1) << SegmentShift);
 
                 _items[_items.Length - 1] = new T[lastPageSize];
                 _length = length;
@@ -93,10 +105,8 @@ namespace Loretta.CodeAnalysis.Collections
 
         public ref T this[int index]
         {
-            get
-            {
-                return ref _items[index >> s_segmentShift][index & s_offsetMask];
-            }
+            [MethodImpl(SegmentedArrayHelper.FastPathMethodImplOptions)]
+            get => ref _items[index >> SegmentShift][index & OffsetMask];
         }
 
         int ICollection.Count => Length;
@@ -116,15 +126,15 @@ namespace Loretta.CodeAnalysis.Collections
         object? IList.this[int index]
         {
             get => this[index];
-            set => this[index] = (T)value!;
+            set => this[index] = (T) value!;
         }
 
         public object Clone()
         {
-            var items = (T[][])_items.Clone();
+            var items = (T[][]) _items.Clone();
             for (var i = 0; i < items.Length; i++)
             {
-                items[i] = (T[])items[i].Clone();
+                items[i] = (T[]) items[i].Clone();
             }
 
             return new SegmentedArray<T>(Length, items);
@@ -134,7 +144,7 @@ namespace Loretta.CodeAnalysis.Collections
         {
             for (var i = 0; i < _items.Length; i++)
             {
-                _items[i].CopyTo(array, index + (i * s_segmentSize));
+                _items[i].CopyTo(array, index + (i * SegmentSize));
             }
         }
 
@@ -143,7 +153,7 @@ namespace Loretta.CodeAnalysis.Collections
             for (var i = 0; i < _items.Length; i++)
             {
                 ICollection<T> collection = _items[i];
-                collection.CopyTo(array, arrayIndex + (i * s_segmentSize));
+                collection.CopyTo(array, arrayIndex + (i * SegmentSize));
             }
         }
 
@@ -206,7 +216,7 @@ namespace Loretta.CodeAnalysis.Collections
                 var index = list.IndexOf(value);
                 if (index >= 0)
                 {
-                    return index + i * s_segmentSize;
+                    return index + i * SegmentSize;
                 }
             }
 
@@ -221,7 +231,7 @@ namespace Loretta.CodeAnalysis.Collections
                 var index = list.IndexOf(value);
                 if (index >= 0)
                 {
-                    return index + i * s_segmentSize;
+                    return index + i * SegmentSize;
                 }
             }
 
@@ -295,7 +305,7 @@ namespace Loretta.CodeAnalysis.Collections
             if (!(other is SegmentedArray<T> o))
                 return false;
 
-            if ((object)_items == o._items)
+            if ((object) _items == o._items)
                 return true;
 
             if (Length != o.Length)
@@ -392,7 +402,7 @@ namespace Loretta.CodeAnalysis.Collections
                 _array = array;
             }
 
-            public static int SegmentSize => s_segmentSize;
+            public static int SegmentSize => SegmentedArray<T>.SegmentSize;
 
             public T[][] Items => _array._items;
         }
